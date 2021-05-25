25268726f717a57156803213356d212cfecdf685
Luuk van Oijen <lazyluuk.channel@gmail.com>
2021-05-25 23:43:43 +0200

    basic error throwing and argument passing

--- FroggyBot/Modules/AdminModule.cs
+++ FroggyBot/Modules/AdminModule.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 
 using Discord;
@@ -13,9 +14,16 @@ namespace FroggyBot.Modules
     {
         [RequireUserPermission(GuildPermission.KickMembers)]
         [Command("warn")]
-        public async Task WarnAsync(IUser user = null)
+        public async Task WarnAsync(IUser user = null, string reason = null) //Will take a user id as well
         {
+            reason = reason ?? "No reason provided.";
             var msg = $"Guild ID: {Context.guildItem.Id}";
+
+            if (user == null) {
+                msg = $"Please provide a user and a reason. Command format: `{Context.guildItem.prefix}warn <@user/id> [reason]`";
+                throw new ArgumentNullException("WarnSync", msg);
+            }
+
             await ReplyAsync(msg);
         }
     }
